9d383228da50c7e0210459d9998161a509dadb66
Prasad Madusanka <prasadmad96@gmail.com>
2020-04-05 04:45:58 +0530

    added db connection

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
+const User = require("./models/user");
+
 const app = express();
 
 //routes
@@ -11,9 +15,47 @@ const errcontroller = require("./controllers/error");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  User.findById("5e89116d64547e20500fba3a")
+    .then((user) => {
+      req.user = user;
+      // console.log(req.user);
+      next();
+    })
+    .catch((err) => {
+      console.log("error");
+    });
+});
+
+//admin routes
 app.use("/admin", adminRoutes);
 
 //404 error
 app.use(errcontroller.get404);
 
-app.listen(3001);
+mongoose
+  .connect(
+    "mongodb+srv://user:user@cluster0-kiwz1.mongodb.net/online-shopping?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "John",
+          email: "john@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+      console.log(
+        "connected-----------------------------------------------------"
+      );
+    });
+    app.listen(3001);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userShema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  cart: {
+    items: [
+      {
+        productId: { type: Schema.Types.ObjectId, required: true },
+        quantity: { type: Number, required: true }
+      }
+    ]
+  }
+});
+
+module.exports = mongoose.model("User", userShema);
